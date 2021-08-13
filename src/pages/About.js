24f8bb74c9e106dfeae557c1d4f3a95afc7cd0e7
timotheJ <timothe.joubert@hotmail.fr>
2021-08-13 12:45:53 +0200

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Header from './Header'
+
+
+const About = () => {
+  return (
+    <Header pageTitle="About page">
+      <p>Content de about page</p>
+    </Header>
+  )
+}
+export default About
